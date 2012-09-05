6555aed37eff81910326e8e14ac148a4430a9cc9
Robert Pelger <robert.pelger@comsysto.com>
2012-09-05 16:25:29 +0200

    implementation of PartyService interface

---
+++ server/src/main/java/com/comsysto/findparty/web/PartyServiceImpl.java
@@ -0,0 +1,39 @@
+package com.comsysto.findparty.web;
+
+import java.util.Set;
+
+import com.comsysto.findparty.Party;
+
+public class PartyServiceImpl implements PartyService {
+
+	@Override
+	public Set<Party> searchParties(Double lon, Double lat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Party showDetails(String partyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void cancelParty(String partyId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void joinParty(String username, String partyId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createParty(Party party) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
