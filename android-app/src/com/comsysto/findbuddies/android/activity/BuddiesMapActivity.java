fdc2e55ca1670432dc9998dd42e69e1c00ea6c0e
hoheiselt <tim.hoheisel@comsysto.com>
2013-06-05 14:47:27 +0200

    use locationService in BuddiesMapActivity

--- android-app/src/com/comsysto/findbuddies/android/activity/BuddiesMapActivity.java
+++ android-app/src/com/comsysto/findbuddies/android/activity/BuddiesMapActivity.java
@@ -1,13 +1,23 @@
 package com.comsysto.findbuddies.android.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import com.comsysto.findbuddies.android.R;
+import com.comsysto.findbuddies.android.service.LocationInfo;
+import com.comsysto.findbuddies.android.service.LocationRequester;
+import com.comsysto.findbuddies.android.service.LocationService;
+import com.comsysto.findparty.Point;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Observable;
+import java.util.Observer;
 
 
 /**
@@ -17,11 +27,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
  * Time: 15:42
  * To change this template use File | Settings | File Templates.
  */
-public class BuddiesMapActivity extends AbstractActivity {
+public class BuddiesMapActivity extends AbstractActivity implements LocationRequester, Observer {
 
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
     static final LatLng KIEL = new LatLng(53.551, 9.993);
     private GoogleMap map;
+    private LocationService locationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +41,7 @@ public class BuddiesMapActivity extends AbstractActivity {
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
                 .getMap();
 
-        if (map!=null){
+        if (map != null) {
             Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
                     .title("Hamburg"));
             Marker kiel = map.addMarker(new MarkerOptions()
@@ -40,6 +51,52 @@ public class BuddiesMapActivity extends AbstractActivity {
                     .icon(BitmapDescriptorFactory
                             .fromResource(R.drawable.androidmarker)));
         }
+        map.setMyLocationEnabled(true);
 
+
+
+
+        locationService = new LocationService(getApplicationContext(), this);
+        locationService.activate();
+    }
+
+    private void zoomToMyLocation(final Point location) {
+
+        double mapLongitude = location.getLon();
+        double mapLatitude = location.getLat();
+
+        Log.d("FindPartiesMapActivity", "Current map center lon/lat : " + mapLongitude + " / " + mapLatitude);
+
+        CameraUpdate center =
+                CameraUpdateFactory.newLatLng(new LatLng(mapLatitude,
+                        mapLongitude));
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+
+        map.moveCamera(center);
+        map.animateCamera(zoom);
+
+//        FindPartiesMapActivity.this.parties = getPartyManagerApplication().getPartyService().searchParties(mapLongitude, mapLatitude, SEARCH_DISTANCE);
+
+
+    }
+
+    @Override
+    public void updateLocationPoint(Point point, String address) {
+        //TODO: Refactor LoccationService - separate location/address update
+    }
+
+    @Override
+    public void updateLocationAddress(String address) {
+        //TODO: Refactor LoccationService - separate location/address update
+    }
+
+    @Override
+    public void update(Observable observable, Object data) {
+        locationService.deactivate();
+        LocationInfo locationInfo = (LocationInfo) data;
+        Point location = new Point();
+        location.setLat(locationInfo.getLatitude());
+        location.setLon(locationInfo.getLongitude());
+        zoomToMyLocation(location);
     }
 }
