183effc8ea11aee5a42955315a714a8538bb261e
Stefan Djurasic <stefan.djurasic@comsysto.com>
2013-06-06 15:55:38 +0200

    adapting menu

--- android-app/src/com/comsysto/findbuddies/android/activity/BuddiesMapActivity.java
+++ android-app/src/com/comsysto/findbuddies/android/activity/BuddiesMapActivity.java
@@ -9,6 +9,7 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -243,5 +244,11 @@ public class BuddiesMapActivity extends AbstractActivity implements
         loadPartiesAndShowOnMap(latLng);
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.search_parties_menu_item).setVisible(false);
+        return true;
+
+    }
 
 }
