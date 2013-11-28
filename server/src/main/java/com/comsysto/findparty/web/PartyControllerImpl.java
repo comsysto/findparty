3fd3099819f441ee8dab056cd5919066fc0cf21a
hoheiselt <tim.hoheisel@comsysto.com>
2013-11-28 17:30:06 +0100

    Removed unnecessary imports.

--- server/src/main/java/com/comsysto/findparty/web/PartyControllerImpl.java
+++ server/src/main/java/com/comsysto/findparty/web/PartyControllerImpl.java
@@ -2,14 +2,11 @@ package com.comsysto.findparty.web;
 
 import com.comsysto.findparty.Party;
 import com.comsysto.findparty.exceptions.InvalidRequestException;
-import com.comsysto.findparty.exceptions.ResourceNotFoundException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
